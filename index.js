029f61c3dd10937ac03cc36fcbf530d46ed9537a
Faisal Ahmed <faisal@techcarebd.com>
2018-09-03 17:47:46 +0600

    mongoose added

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const express =require('express');
-const app=express();
-app.get('/',(req,res)=>res.send('Hello TechCare !'));
-app.listen(3000,()=>console.log('techcare is listening to 3000!'));
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', ()=> {
+  //  console.log('mongoose running successfully')
+  var kittySchema = new mongoose.Schema({
+    name: String
+  });
+});
+
+var database;
+MongoClient.connect('mongodb://localhost:27017/userInformation', (err, client) => {
+    if (err) return console.log(err)
+    database = client.db('userInformation') // whatever your database name is
+    app.listen(8000, () => {
+        console.log('TechCare listening to  8000')
+    })
+})
+app.get('/', (req, res) => {
+    database.collection('UserInformation').find().toArray((err, result) => {
+        if (err)
+            console.log('error : ', err);
+        else {
+            //console.log('result : ', result);
+            res.send('Hello TechCare !');
+        }
+
+    })
+}
+);
+
 app.use(express.static('public'));
