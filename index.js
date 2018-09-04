f45251861d63bf7f596b654d11efba100ab2a1da
Faisal Ahmed <faisal@techcarebd.com>
2018-09-04 12:14:19 +0600

    function converted to array function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-var nwClient = new Models.client({ name: 'Onnorokom Group' });
+var nwClient = new Models.client({ name: 'Onnorokom Groups' });
 nwClient.save(error => {
     if (error)
         console.error(error)
--- models.js
+++ models.js
@@ -4,8 +4,8 @@ mongoose.connect('mongodb://localhost:27017/techcare-website');
 var db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Connection error:"));
-db.once("open", function (callback) {
-    console.log("Connection Succeeded."); 
+db.once("open", (callback) => {
+    console.log("Connection Succeeded.");
 });
 var client = mongoose.model('client', ClientSchema.clientSchema);
 module.exports.client = client;
