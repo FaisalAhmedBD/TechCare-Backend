8ed2fb30138923a7d47ccbd84e30fe5b97bed096
Faisal Ahmed <faisal@techcarebd.com>
2018-09-04 16:10:53 +0600

    data retrieved from database

--- index.js
+++ index.js
@@ -3,20 +3,32 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-var nwClient = new Models.client({
-    name: 'Rokomari.com',
-    product_name: 'Rokomari Android App',
-    type: 'Ecommerce',
-    location: 'Bangladesh',
-    feedback: 'Awesome product',
-    client_logo: 'client logo test!',
-    product_logo: 'product logo test',
-});
-nwClient.save(error => {
-    if (error)
-        console.error(error)
-    console.log('new client added');
-})
+// var nwClient = new Models.client({
+//     name: 'Rokomari.com',
+//     product_name: 'Rokomari Android App',
+//     type: 'Ecommerce',
+//     location: 'Bangladesh',
+//     feedback: 'Awesome product',
+//     client_logo: 'client_logo_url',
+//     product_logo: 'product_logo_url',
+// });
+// nwClient.save(error => {
+//     if (error)
+//         console.error(error)
+//     console.log('new client added');
+// })
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var clientList;
+Models.client.find({}, (err, data) => {
+    if (err)
+        console.log('error')
+    else {
+        clientList=data;
+        console.log(data)
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send(clientList)
+})
 app.listen(8000, () => console.log('TechCare is listening on port 8000!'))
