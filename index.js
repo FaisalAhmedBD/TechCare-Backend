ddccd1320d3574630ef3eb07ff2413a510a10ab9
Faisal Ahmed <faisal@techcarebd.com>
2018-09-04 11:24:21 +0600

    document is saving to collection using mongoose

--- index.js
+++ index.js
@@ -2,35 +2,30 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost:27017/techcare-website');
+app.use(express.static('public'));
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', ()=> {
-  //  console.log('mongoose running successfully')
-  var kittySchema = new mongoose.Schema({
-    name: String
-  });
+db.once('open', () => {
+    console.log('mongoose running successfully')
+    var clientSchema = new mongoose.Schema({
+        name: String
+    });
+    clientSchema.methods.speak = function () {
+        var greeting = this.name
+            ? "Meow name is " + this.name
+            : "I don't have a name";
+        console.log(greeting);
+    }
+    var client = mongoose.model('client', clientSchema);
+    var nwClient = new client({ name: 'Onnorokom Group' });
+    nwClient.save(error => {
+        if (error)
+            console.error(error)
+        console.log('new client added');
+    })
+    console.log('our new client is : ', nwClient.name);
+    nwClient.speak();
+
 });
-
-var database;
-MongoClient.connect('mongodb://localhost:27017/userInformation', (err, client) => {
-    if (err) return console.log(err)
-    database = client.db('userInformation') // whatever your database name is
-    app.listen(8000, () => {
-        console.log('TechCare listening to  8000')
-    })
-})
-app.get('/', (req, res) => {
-    database.collection('UserInformation').find().toArray((err, result) => {
-        if (err)
-            console.log('error : ', err);
-        else {
-            //console.log('result : ', result);
-            res.send('Hello TechCare !');
-        }
-
-    })
-}
-);
-
-app.use(express.static('public'));
