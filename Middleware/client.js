2b72f0428c6ed8b26c44ad118f303df50e70dc02
Faisal Ahmed <faisal@techcarebd.com>
2018-09-05 10:15:01 +0600

    exporting mutple module

--- Middleware/client.js
+++ Middleware/client.js
@@ -1,6 +1,21 @@
-var express=require('express');
-var app=express();
-var Models=require('../Model/models');
+const express = require('express');
+const app = express();
+var Models = require('../Model/models');
+
+// var nwClient = new Models.client({
+//     name: 'Rokomari.com',
+//     product_name: 'Rokomari Android App',
+//     type: 'Ecommerce',
+//     location: 'Bangladesh',
+//     feedback: 'Awesome product',
+//     client_logo: 'client_logo_url',
+//     product_logo: 'product_logo_url',
+// });
+// let addClient=nwClient.save(error => {
+//     if (error)
+//         console.error(error)
+//     console.log('new client added');
+// })
 
 var clientList;
 Models.client.find({}, (err, data) => {
@@ -12,10 +27,14 @@ Models.client.find({}, (err, data) => {
     }
 });
 
-let clientMiddleWare=(req, res) => {
+let clientMiddleWare = (req, res) => {
     res.send({
         'clients': clientList,
         'number_of_clients': clientList.length,
     })
 }
-module.exports.clientMiddleWare=clientMiddleWare;
+module.exports=
+{
+    clientMiddleWare:clientMiddleWare,
+   // addClient:addClient
+}
--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-var Models = require('./Model/models')
-var clientMiddleWare=require('./Middleware/client');
+var clientMiddleWare = require('./Middleware/client');
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-// var nwClient = new Models.client({
-//     name: 'Rokomari.com',
-//     product_name: 'Rokomari Android App',
-//     type: 'Ecommerce',
-//     location: 'Bangladesh',
-//     feedback: 'Awesome product',
-//     client_logo: 'client_logo_url',
-//     product_logo: 'product_logo_url',
-// });
-// nwClient.save(error => {
-//     if (error)
-//         console.error(error)
-//     console.log('new client added');
-// })
 
-
-app.get('/', clientMiddleWare.clientMiddleWare)
+app.get('/', clientMiddleWare.clientMiddleWare);
+//app.post('/add-client', clientMiddleWare.addClient);
 app.listen(8000, () => console.log('TechCare is listening on port 8000!'))
