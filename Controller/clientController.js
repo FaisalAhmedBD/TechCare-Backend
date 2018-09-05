1b9a476a6864d843e80943bc15ccb6bd108c694d
Faisal Ahmed <faisal@techcarebd.com>
2018-09-05 14:13:57 +0600

    controller added

---
+++ Controller/clientController.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+var Models = require('../Model/models');
+var { addClient } = require('../Middleware/addClient');
+
+let clientMiddleWare = (req, res) => {
+    Models.client.find({}, (err, data) => {
+        if (err) {
+            console.log('error : ', err);
+            res.send('No data found')
+        }
+        else {
+            res.send({
+                'clients': data,
+                'number_of_clients': data.length,
+            })
+        }
+    });
+}
+module.exports = {
+    clientMiddleWare,
+    addClient
+}
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var clientMiddleWare = require('./Middleware/client');
+var { clientMiddleWare, addClient } = require('./Controller/clientController');
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
+//app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.get('/', clientMiddleWare.clientMiddleWare);
-app.post('/add-client', (req,res) => {
-    clientMiddleWare.addClient.save(error => {
+app.get('/', clientMiddleWare);
+app.post('/add-client', (req, res) => {
+    addClient.save(error => {
         if (error)
             console.error(error)
         console.log('new client added');
