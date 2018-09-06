c7a65ea5dfc64c1e14f3928990f60b20df0b2ccc
Faisal Ahmed <faisal@techcarebd.com>
2018-09-06 12:24:47 +0600

    client model, schema removed from common folder

--- Router/index.js
+++ Router/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var router_v1 = express.Router();
 var { getClientInfo } = require('./client/clientController');
+
 router_v1.get('/clients', getClientInfo);
 
 module.exports = {
--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var bodyParser = require("body-parser");
 var {router_v1}=require('./Router/index');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/static', express.static(path.join(__dirname, 'public')));
-var { getClientInfo, addNewClient } = require('./Controller/clientController');
-var { getTeamMemberInfo, addnewTeamMember } = require('./Controller/teamMemberController');
-var { getJobPosts, addJobPost } = require('./Controller/carrerController');
-var { getProductInfo, addNewProduct } = require('./Controller/productController');
+// var { getClientInfo, addNewClient } = require('./Controller/clientController');
+// var { getTeamMemberInfo, addnewTeamMember } = require('./Controller/teamMemberController');
+// var { getJobPosts, addJobPost } = require('./Controller/carrerController');
+// var { getProductInfo, addNewProduct } = require('./Controller/productController');
 
 app.get('/', (req, res) => {
     res.send('Hello TechCare !!');
@@ -16,13 +16,13 @@ app.get('/', (req, res) => {
 app.use('/v1',router_v1);
 
 //app.get('/clients', getClientInfo);
-app.get('/team-members', getTeamMemberInfo);
-app.get('/job-posts', getJobPosts);
-app.get('/products', getProductInfo);
+// app.get('/team-members', getTeamMemberInfo);
+// app.get('/job-posts', getJobPosts);
+// app.get('/products', getProductInfo);
 
-app.post('/add-client', addNewClient);
-app.post('/add-team-member', addnewTeamMember);
-app.post('/add-job-post', addJobPost);
-app.post('/add-new-product', addNewProduct);
+// app.post('/add-client', addNewClient);
+// app.post('/add-team-member', addnewTeamMember);
+// app.post('/add-job-post', addJobPost);
+// app.post('/add-new-product', addNewProduct);
 
 app.listen(8000, () => console.log('TechCare is listening on port 8000!'))
