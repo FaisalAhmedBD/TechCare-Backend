6b49d677fb6d353ac849f6f01c51029eb24c50ef
Faisal Ahmed <faisal@techcarebd.com>
2018-09-06 10:23:47 +0600

    product api implemented

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 var { getClientInfo, addNewClient } = require('./Controller/clientController');
 var { getTeamMemberInfo, addnewTeamMember } = require('./Controller/teamMemberController');
 var { getJobPosts, addJobPost } = require('./Controller/carrerController');
-
+var { getProductInfo, addNewProduct } = require('./Controller/productController');
 app.get('/', (req, res) => {
     res.send('Hello TechCare !!');
 })
@@ -15,8 +15,11 @@ app.get('/', (req, res) => {
 app.get('/clients', getClientInfo);
 app.get('/team-members', getTeamMemberInfo);
 app.get('/job-posts', getJobPosts);
+app.get('/products', getProductInfo);
+
 app.post('/add-client', addNewClient);
 app.post('/add-team-member', addnewTeamMember);
 app.post('/add-job-post', addJobPost);
+app.post('/add-new-product', addNewProduct);
 
 app.listen(8000, () => console.log('TechCare is listening on port 8000!'))
